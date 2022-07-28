7c3daaf345cc60918c3f679fedb2a5f577667fd0
Bohdan <sidorchuk.bodia@gmail.com>
2022-07-29 00:33:55 +0300

    228335447

---
+++ data_type.py
@@ -0,0 +1,32 @@
+
+# number_a = 7     #straight method
+# number_b = number_a   #reference to another object
+#
+#
+# print(number_a, type(number_a))
+#
+# print(number_b, type(number_b))
+
+# a=5
+# print(a, type(a))
+#
+# b=5+7
+# print(b, type(b))
+
+# b= "basketball"
+# print(b, type(b))
+#
+# c= "football"
+# print(c, type(c))
+#
+# d= "voleyball"
+# print(d, type(d))
+#
+# e= "table tennis"
+# print(e, type(e))
+#
+# f= "tennis"
+# print(f, type(f))
+#
+# result = b + c + d + e + f
+# print(result, type(result))
