44ef4786194b96ea68c35a6825d32ced4d6cf4c4
Bohdan <sidorchuk.bodia@gmail.com>
2022-07-29 03:13:31 +0300

    335

--- rows_and_numbers.py
+++ rows_and_numbers.py
@@ -1,23 +1,23 @@
-username = input("Please tell me your name ")
-result = f"Hello {username}, nice to e-meet you!"
-
-print(result)
-username = input("Please tell me any number ")
-result = f"Oh this is {username}, thank u "
-
-print(result)
-
-namber = str(2.2)
-print(namber)
-
-value_1 = int(2.2)  #removes decimal, doesn't round
-print(value_1, type(value_1))
-
-exponentiation = value_1 ** 4
-print(exponentiation, type(exponentiation))
-
-square_root = value_1 ** 0.5
-print(square_root, type(square_root))
-
-division_remainder = value_1 % 2
-print(division_remainder, type(division_remainder))
+# username = input("Please tell me your name ")
+# result = f"Hello {username}, nice to e-meet you!"
+#
+# print(result)
+# username = input("Please tell me any number ")
+# result = f"Oh this is {username}, thank u "
+#
+# print(result)
+#
+# namber = str(2.2)
+# print(namber)
+#
+# value_1 = int(2.2)  #removes decimal, doesn't round
+# print(value_1, type(value_1))
+#
+# exponentiation = value_1 ** 4
+# print(exponentiation, type(exponentiation))
+#
+# square_root = value_1 ** 0.5
+# print(square_root, type(square_root))
+#
+# division_remainder = value_1 % 2
+# print(division_remainder, type(division_remainder))
---
+++ rows_and_numbers_advance.py
@@ -0,0 +1,16 @@
+# username = input("Please tell me your name")
+# result = f"Hello {username}, nice to e-meet u"
+#
+# print(result)
+
+#
+# text = "boHDan"
+# print(text.capitalize())
+
+# text = "    boHDan    "
+# print(text.strip())
+# text = "boHDan"
+# print(text, len(text))
+#
+# text = "boHDan"
+# print(text.swapcase())
