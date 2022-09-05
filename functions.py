723316a1356cc5ccb92807df5f475687d1e0992d
Bohdan <sidorchuk.bodia@gmail.com>
2022-09-06 00:09:23 +0300

    eazy-e

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@
 import datetime
 
 
-"""first exercice"""
+"""1 exercice"""
 start = int(input('enter the start of calculation: '))
 finish = int(input('enter the start of calculation: '))
 
@@ -15,14 +15,15 @@ def counter(start: int, finish: int) -> int:
 
 print(counter(start, finish))
 
-"""second exercice"""
+"""2 exercice"""
 
 def time_convert(sec: int):
     result = datetime.timedelta(seconds=sec)
     return result
-print(time_convert(360000))
+print( time_convert (360000) )
 
-"""third exercise"""
+
+"""3 exercise"""
 list = [1, 3, 4, 6, 7, 8, 12, 2]
 
 def for_counter(list: list) -> int or float:
@@ -38,7 +39,7 @@ list = [1, 3, 4, 6, 7, 8, 12, 2]
 def while_counter(list: list) -> int or float:
     sum = 0
     while len(list) != 0:
-        sum += list.pop()
+        sum += list()
     return sum
 
 print(while_counter(list))
@@ -55,3 +56,73 @@ def recurs_counter(list: list) -> int or float:
 
 print(recurs_counter(list))
 
+def time_convert(sec: int):
+    result = datetime.timedelta(seconds=sec)
+    return result
+print( time_convert (360000) )
+
+"""4 exersice"""
+
+
+def recurs_fibo(n: int) -> int:
+    if n in (1, 2):
+        return 1
+    return recurs_fibo(n - 1) + recurs_fibo(n - 2)
+
+
+print(f'{recurs_fibo(5) = }')
+
+
+"""5 exersice"""
+
+"""first decor"""
+
+print('first decor:')
+
+
+def ingr_burger(decor_this) -> any:
+    def ingr_tomato():
+        print('tomato')
+
+    def ingr_meat():
+        print('meat')
+
+    def ingr_chees():
+        print('chees')
+
+    def ingr_bread():
+        print('bread')
+    return ingr_tomato(), ingr_meat(), ingr_chees(), ingr_bread()
+
+
+@ingr_burger
+def burger() -> None:
+    return
+
+"""second decor"""
+
+print('second decor:')
+
+
+def ingr_tomato(decor_this):
+    print('tomato')
+
+
+def ingr_meat(decor_this):
+    print('meat')
+
+
+def ingr_chees(decor_this):
+    print('chees')
+
+
+def ingr_bread(decor_this):
+    print('bread')
+
+
+@ingr_bread
+@ingr_chees
+@ingr_meat
+@ingr_tomato
+def burger() -> None:
+    return
