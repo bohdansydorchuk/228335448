81434a14931e4360696e63898d03530d43aec2cb
Bohdan <sidorchuk.bodia@gmail.com>
2022-09-13 19:35:39 +0300

    budaog

---
+++ virtualenv_practice.py
@@ -0,0 +1,24 @@
+"""виртуальные команды
+
+ mkdir py_venv
+
+ cd py_venv
+
+ python -m venv CV_venv
+
+ cd CV_venv
+
+ cd Scripts
+
+ activate.bat
+
+ pip install django numpy pandas requests SQLAlchemy flask
+
+ pip freeze
+
+ pip freeze > requirements.txt
+
+ deactivate
+
+ """
+
